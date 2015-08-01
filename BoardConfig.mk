TARKZiM/android_device_htc_e8-twrp

Move recovery files to recovery/root
Make init E8-compatible (for E8 lollipop update)
dtb already includes E8 entries

PS2: Update init for additional E8 entries
PS3: Fix missing parenthesis
PS4: Update custom init.rc from stock TWRP
PS5: Remove prebuilt init.rc

Change-Id: I12068fec65b1b899135e9ab20dad5971b2d823bc

BoardConfig.mk
@@ -53,8 +53,8 @@ BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x02008000 --dt device/htc/m8/recovery/kernel/dt.img --tags_offset 0x01e00000
-BOARD_CUSTOM_BOOTIMG_MK := device/htc/m8/recovery/kernel/mkbootimg.mk
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x02008000 --dt device/htc/m8/recovery/dt.img --tags_offset 0x01e00000
+BOARD_CUSTOM_BOOTIMG_MK := device/htc/m8/recovery/mkbootimg.mk
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 274464768
@@ -72,8 +72,7 @@ BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_RECOVERY_SWIPE := true
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
 BOARD_USES_MMCUTILS := true
-TARGET_PREBUILT_KERNEL := device/htc/m8/recovery/kernel/kernel
-TARGET_RECOVERY_INITRC := device/htc/m8/recovery/etc/init.rc
+TARGET_PREBUILT_KERNEL := device/htc/m8/recovery/kernel
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
 

schqiushui/android_device_htc_e8

Conflicts:
	BoardConfig.mk
	init/init_e8.c
	recovery/dt.img

BoardConfig.mk
@@ -53,8 +53,8 @@ BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x02008000 --dt device/htc/e8/recovery/kernel/dt.img --tags_offset 0x01e00000
-BOARD_CUSTOM_BOOTIMG_MK := device/htc/e8/recovery/kernel/mkbootimg.mk
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x02008000 --dt device/htc/e8/recovery/dt.img --tags_offset 0x01e00000
+BOARD_CUSTOM_BOOTIMG_MK := device/htc/e8/recovery/mkbootimg.mk
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 274464768
@@ -72,8 +72,7 @@ BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_RECOVERY_SWIPE := true
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
 BOARD_USES_MMCUTILS := true
-TARGET_PREBUILT_KERNEL := device/htc/e8/recovery/kernel/kernel
-TARGET_RECOVERY_INITRC := device/htc/e8/recovery/etc/init.rc
+TARGET_PREBUILT_KERNEL := device/htc/e8/recovery/kernel
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
 
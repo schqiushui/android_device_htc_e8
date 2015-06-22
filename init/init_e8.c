0bad848e5585720a0165ca482b035ade10c383f7
Captain Throwback <captainthrowback@hotmail.com>
2015-06-22 13:03:26 -0400

    Move recovery files to recovery/root
    Make init E8-compatible (for E8 lollipop update)
    dtb already includes E8 entries
    
    PS2: Update init for additional E8 entries
    PS3: Fix missing parenthesis
    PS4: Update custom init.rc from stock TWRP
    PS5: Remove prebuilt init.rc
    
    Change-Id: I12068fec65b1b899135e9ab20dad5971b2d823bc
    
    Conflicts:
    	BoardConfig.mk
    	init/init_e8.c
    	recovery/dt.img

--- init/init_e8.c
+++ init/init_e8.c
@@ -53,27 +53,84 @@ void vendor_load_properties()
 
     property_get("ro.boot.mid", bootmid);
 
-    if (strstr(bootmid, "0PAJ50000")) {
+    if (strstr(bootmid, "0P6B20000")) {
+        /* m8vzw (m8wl) */
+        cdma_properties("0", "10", "310012", "Verizon");
+        property_set("ro.product.device", "m8wl");
+        property_set("ro.product.model", "HTC6525LVW");
+        property_set("ro.build.product", "m8wl");
+        property_set("ro.ril.vzw.feature", "1");
+        property_set("ro.ril.oem.ecclist", "911,*911,#911");
+        property_set("ro.ril.enable.a52", "0");
+        property_set("ro.ril.enable.dtm", "0");
+        property_set("ro.ril.gprsclass", "12");
+        property_set("ro.ril.att.feature", "0");
+        property_set("ro.ril.enable.managed.roaming", "1");
+        property_set("ro.ril.oem.show.act", "0");
+        property_set("ro.ril.set.mtusize", "1428");
+        property_set("ro.ril.air.enabled", "1");
+        property_set("ro.ril.wp.feature", "1");
+        property_set("ro.cdma.data_retry_config", "max_retries=infinite,0,0,60000,120000,480000,900000");
+        property_set("ro.gsm.data_retry_config", "max_retries=infinite,0,0,60000,120000,480000,900000");
+        property_set("ro.gsm.2nd_data_retry_config", "max_retries=infinite,0,0,60000,120000,480000,900000");
+        property_set("ro.ril.gsm.to.lte.blind.redir", "1");
+        property_set("ro.config.svlte1x", "true");
+        property_set("ro.ril.def.agps.mode", "6");
+    } else if (strstr(bootmid, "0P6B70000")) {
+        /* m8spr (m8whl) */
+        cdma_properties("1", "8", "310120", "Sprint");
+        property_set("ro.product.device", "m8whl");
+        property_set("ro.product.model", "831C");
+        property_set("ro.build.product", "m8whl");
+        property_set("telephony.sms.pseudo_multipart", "1");
+        property_set("ro.ril.oem.ecclist", "911");
+        property_set("ro.ril.enable.pre_r8fd=1", "1");
+        property_set("ro.ril.enable.sdr", "0");
+        property_set("ro.ril.set.mtusize", "1422");
+    } else if (strstr(bootmid, "0P6B64000")) {
+        /* m8dugl */
+        property_set("ro.build.product", "m8dugl");
+        property_set("ro.product.device", "m8dugl");
+        property_set("ro.product.model", "HTC One_M8 dual sim");
+        property_set("ro.telephony.default_network", "9");
+    } else if (strstr(bootmid, "0PAJ50000")) {
         /* Sprint (mecwhl) */
         cdma_properties("1", "8", "310120", "Sprint");
+        property_set("ro.build.product", "htc_mecwhl");
         property_set("ro.product.device", "htc_mecwhl");
         property_set("ro.product.model", "0PAJ5");
     } else if (strstr(bootmid, "0PAJ10000")) {
         /* China Mobile (mectl) */
+        property_set("ro.build.product", "htc_mectl");
         property_set("ro.product.device", "htc_mectl");
         property_set("ro.product.model", "HTC One_E8");
-    } else if (strstr(bootmid, "0PAJ20000") || strstr(bootmid, "0PAJ21000")) {
+    } else if (strstr(bootmid, "0PAJ20000") || strstr(bootmid, "0PAJ21000") || strstr(bootmid, "0PAJ22000")) {
         /* China Unicom/Bangladesh (mecdugl) */
+        property_set("ro.build.product", "htc_mecdugl");
         property_set("ro.product.device", "htc_mecdugl");
         property_set("ro.product.model", "HTC One_E8 Dual Sim");
+    } else if (strstr(bootmid, "0PAJ30000")) {
+        /* Europe (mecul_emea) */
+        property_set("ro.build.product", "htc_mecul_emea");
+        property_set("ro.product.device", "htc_mecul_emea");
+        property_set("ro.product.model", "HTC One_E8");
+    } else if (strstr(bootmid, "0PAJ31000")) {
+        /* Singapore/Vietnam/Europe MMR (mecul) */
+        property_set("ro.build.product", "htc_mecul");
+        property_set("ro.product.device", "htc_mecul");
+        property_set("ro.product.model", "HTC One_E8");
     } else if (strstr(bootmid, "0PAJ40000")) {
         /* China Telecom (mecdwgl) */
+        property_set("ro.build.product", "htc_mecdwgl");
         property_set("ro.product.device", "htc_mecdwgl");
         property_set("ro.product.model", "HTC One_E8 Dual Sim");
     } else {
-        /* E8 (mecul) */
-        property_set("ro.product.device", "htc_mecul");
-        property_set("ro.product.model", "HTC One_E8");
+        /* m8 */
+        property_set("ro.build.product", "m8");
+        property_set("ro.product.device", "m8");
+        property_set("ro.product.model", "HTC One_M8");
+        property_set("ro.telephony.default_network", "9");
+        property_set("telephony.lteOnGsmDevice", "1");
     }
 
     property_get("ro.product.device", device);
@@ -99,7 +156,7 @@ void cdma_properties(char default_cdma_sub[], char default_network[],
     property_set("ro.ril.enable.sdr", "0");
     property_set("ro.ril.enable.gea3", "1");
     property_set("ro.ril.enable.a53", "1");
-    property_set("ro.ril.enable.r8fd", "1");
+    property_set("ro.ril.enable.r8fd=1", "1");
     property_set("persist.radio.snapshot_enabled", "1");
     property_set("persist.radio.snapshot_timer", "22");
 }
