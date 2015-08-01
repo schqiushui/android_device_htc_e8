TARKZiM/android_device_htc_e8-twrp

Fix offmode charging
Got a bit too aggressive with my cleanup....

Change-Id: I276b4fcb8a7694f5cc3b5003e2d7c58797800d78

BoardConfig.mk
@@ -73,6 +73,7 @@ BOARD_RECOVERY_SWIPE := true
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
 BOARD_USES_MMCUTILS := true
 TARGET_PREBUILT_KERNEL := device/htc/m8/recovery/kernel
+TARGET_RECOVERY_INITRC := device/htc/m8/recovery/init.rc
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
 

schqiushui/android_device_htc_e8

Conflicts:
	BoardConfig.mk

BoardConfig.mk
@@ -73,6 +73,7 @@ BOARD_RECOVERY_SWIPE := true
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
 BOARD_USES_MMCUTILS := true
 TARGET_PREBUILT_KERNEL := device/htc/e8/recovery/kernel
+TARGET_RECOVERY_INITRC := device/htc/e8/recovery/init.rc
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
 