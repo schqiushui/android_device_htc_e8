164308791f2dc3d92a199f5df9fb78a5f79e447e
Captain Throwback <captainthrowback@hotmail.com>
2015-11-13 15:00:58 -0500

    Fix Android.mk
    Change-Id: I3cf68a575b9c39e5573f5b5b0c9df672d1039ed3

--- Android.mk
+++ Android.mk
@@ -14,9 +14,10 @@
 # limitations under the License.
 #
 
+ifeq ($(TARGET_DEVICE),e8)
+
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),e8)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
